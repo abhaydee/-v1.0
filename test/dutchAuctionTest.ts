8746c230c0cedb7b019388e21146d1a28e48a7f0
abhaydee <abhaygirish1997@gmail.com>
2023-02-02 20:35:22 -0500

    formatting code using prettier

--- test/dutchAuctionTest.ts
+++ test/dutchAuctionTest.ts
@@ -1,48 +1,35 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 describe("Basic Dutch Auction Contract", function () {
-    let contract:any;
-    let owner:any;
-    let wallet: any;
+  let contract: any;
+  let owner: any;
+  let wallet: any;
 
-    beforeEach("testing the smart contract", async function(){
-        const [ownerAddress, otherAccount] = await ethers.getSigners();
-        wallet = otherAccount;
-        owner = ownerAddress;
-        const dutchAuction = await ethers.getContractFactory("BasicDutchAuction");
-        const deployContract = await dutchAuction.deploy(100,10,10);
-        contract =await deployContract.deployed();
-    })
+  beforeEach("testing the smart contract", async function () {
+    const [ownerAddress, otherAccount] = await ethers.getSigners();
+    wallet = otherAccount;
+    owner = ownerAddress;
+    const dutchAuction = await ethers.getContractFactory("BasicDutchAuction");
+    const deployContract = await dutchAuction.deploy(100, 10, 10);
+    contract = await deployContract.deployed();
+  });
 
-    it("Test for checking auction end scenario", async function(){
-        expect(await contract.auctionEnded()).to.be.false;
-    })
+  it("Test for checking auction end scenario", async function () {
+    expect(await contract.auctionEnded()).to.be.false;
+  });
 
-    it("testing the bid functionality",async  function(){
-        await contract.connect(wallet).bid({value : 205});
-        expect(await contract.auctionEnded()).to.be.true;
-        expect(await contract.winner()).be.eq(wallet.address);
-    })
+  it("testing the bid functionality", async function () {
+    await contract.connect(wallet).bid({ value: 205 });
+    expect(await contract.auctionEnded()).to.be.true;
+    expect(await contract.winner()).be.eq(wallet.address);
+  });
 
-    it("it should not allow an invalid bid", async function(){
-        try{
-            await contract.bid({value:100});
-            expect.fail();
-        }
-        catch(error:any){
-            expect(error.message).to.be.revertedWith("Insufficient funds.");
-        }
-    })
-
-
-   
-
-
-  
-
-
-    
-
-
-  
-}); 
+  it("it should not allow an invalid bid", async function () {
+    try {
+      await contract.bid({ value: 100 });
+      expect.fail();
+    } catch (error: any) {
+      expect(error.message).to.be.revertedWith("Insufficient funds.");
+    }
+  });
+});
